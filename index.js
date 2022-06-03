e5e5a8c32a4a035a861f18a35cae154abe692877
Julie Boehm <julie.anne.boehm@gmail.com>
2022-06-03 09:17:08 -0600

    commit tree 2511fde15a69135b3489e46a196a655663ac55de

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+//const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+    let newArray = [];
+    for (let i = 0; i < cards.length; i++) {
+       newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+
+}
+
+
+    
+
